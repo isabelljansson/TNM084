0bc4c72f893989b1a47ae8c8e1f4c9ee81454244
isabelljansson <janssonisabell@gmail.com>
2015-12-05 17:30:45 +0100

    simple plane mesh, no shaders yet

---
+++ js/main.js
@@ -0,0 +1,63 @@
+//Isabell Jansson
+
+var scene, renderer, plane;
+
+
+init();
+animate();
+
+
+//initialize scene
+function init() 
+{
+
+	//scene
+	container = document.getElementById( 'container' );
+	scene = new THREE.Scene();
+
+	//camera
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
+    camera.position.set(0, 40, 100);
+	camera.lookAt(scene.position);
+    scene.add(camera);
+
+    //Renderer
+	renderer = new THREE.WebGLRenderer(container);
+    renderer.setClearColor( 0xffffff );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    //light
+    // add subtle ambient lighting
+	var light = new THREE.PointLight(0xffffff);
+	light.position.set(0,250,0);
+	scene.add(light);
+
+
+    //Init plane
+    var geometry = new THREE.PlaneGeometry( 100, 100, 40, 40 );
+    var material = new THREE.MeshBasicMaterial( {color: 0x8888ff} );
+    plane = new THREE.Mesh( geometry, material );
+    plane.position.set(0, -50, -100);
+	scene.add( plane );
+	plane.rotation.x = - Math.PI/2;
+
+	container.innerHTML = "";
+
+    document.body.appendChild( renderer.domElement ); 
+  
+}
+
+
+
+function animate() 
+{
+    requestAnimationFrame( animate );
+	render();		
+
+}
+
+function render() 
+{	
+	renderer.render( scene, camera );
+}
