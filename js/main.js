da105ea09a69c78fa2f437187bfef9dc16e9b1ed
isabelljansson <janssonisabell@gmail.com>
2015-12-15 18:56:32 +0100

    shaders are written in separate files, also playing around with the shaders

--- js/main.js
+++ js/main.js
@@ -1,76 +1,105 @@
 //Isabell Jansson
 
-var scene, renderer, plane, start = Date.now();
+require([
+    "../libs/text!../shaders/vertexShader.glsl",
+    "../libs/text!../shaders/fragmentShader.glsl",
+    "../libs/text!../shaders/simplex-noise-3d.glsl"
+],
 
-
-init();
-animate();
-
-
-//initialize scene
-function init() 
+function (
+    VertexShader,
+    FragmentShader,
+    Noise) 
 {
-
-	//scene
-	container = document.getElementById( 'container' );
-	scene = new THREE.Scene();
-
-	//camera
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
-    camera.position.set(0, 40, 100);
-	camera.lookAt(scene.position);
-    scene.add(camera);
-
-    //Renderer
-	renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor( 0xffffff );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-    //light
-    // add subtle ambient lighting
-	var light = new THREE.PointLight(0xffffff);
-	light.position.set(0,250,0);
-	scene.add(light);
+    "use strict";
 
 
-    //Init plane
-    var geometry = new THREE.PlaneGeometry( 100, 100, 40, 40 );
-    
-    material = new THREE.ShaderMaterial( {
-        //define uniforms to use...
-        uniforms: 
+    var scene, renderer, camera;
+    var plane, material, waterUniforms, waterAttributes, start = Date.now();
+
+
+    init();
+    animate();
+
+
+    //initialize scene
+    function init() 
+    {
+
+    	//scene
+    	container = document.getElementById( 'container' );
+    	scene = new THREE.Scene();
+
+    	//camera
+        camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
+        camera.position.set(0, 40, 100);
+    	camera.lookAt(scene.position);
+        scene.add(camera);
+
+        //Renderer
+    	renderer = new THREE.WebGLRenderer();
+        renderer.setClearColor( 0xffffff );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+        //light
+        // add subtle ambient lighting
+    	var light = new THREE.PointLight(0xffffff);
+    	light.position.set(0,250,0);
+    	scene.add(light);
+
+        /*
+         * WATER
+         */
+
+         //geometry
+        var geometry = new THREE.PlaneGeometry( 200, 200, 100, 100 );
+        
+        //shader variables
+        waterUniforms = 
         { 
             time: 
             {
                 type: "f",  //float
                 value: 0.0  //initialized to 0
             }
-        },
+        }
+        waterAttributes = 
+        {
+            displacement:
+            {
+                type: 'f',  //float
+                value: []   //empty array
+            }
+        }
 
-        vertexShader: document.getElementById( 'vertexShader' ).textContent,
-        fragmentShader: document.getElementById( 'fragmentShader' ).textContent
-    } );
+        //material
+        material = new THREE.ShaderMaterial( 
+        {
+            uniforms: waterUniforms,
+            attributes: waterAttributes,
+            vertexShader: Noise + VertexShader,
+            fragmentShader: FragmentShader
+        } );
 
-    plane = new THREE.Mesh( geometry, material );
-    plane.position.set(0, -50, -100);
-	scene.add( plane );
-	plane.rotation.x = - Math.PI/2;
-
-	container.innerHTML = "";
-
-    document.body.appendChild( renderer.domElement ); 
-  
-}
+        plane = new THREE.Mesh( geometry, material );
+        plane.position.set(0, -50, -100);
+    	scene.add( plane );
+    	plane.rotation.x = - Math.PI/2;
+      
+    	container.innerHTML = "";
+        document.body.appendChild( renderer.domElement );       
+    }
 
 
+    function animate() 
+    {
+        requestAnimationFrame( animate );
+        waterUniforms.time.value +=  0.01;
+        //material.attributes.displacement.value += Math.sin(10.0);
+        //Another way to increase time: = .00025 * ( Date.now() - start );
+        
+    	renderer.render( scene, camera );		
 
-function animate() 
-{
-    requestAnimationFrame( animate );
-    material.uniforms.time.value +=  0.01;
-     //Another way to increase time: = .00025 * ( Date.now() - start );
-    
-	renderer.render( scene, camera );		
-
-}
+    }
+});
--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -1,7 +1,9 @@
 varying vec2 vUv;
+varying vec4 curvePos;
 
 void main() 
 {
-    // color RGBA: u, v, 0.5, 1
-    gl_FragColor = vec4( vec3( vUv, 0.5 ), 1. );
+    //curvePos is passed through the vertex shader
+    // color is RGBA: u, v, 0, 1
+    gl_FragColor = vec4( vec3( vUv, curvePos.y ), 1. );
 }
