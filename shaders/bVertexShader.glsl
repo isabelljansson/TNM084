695ed45c3afeaae5ceb77498ff6f5fc7b3b34947
isabelljansson <janssonisabell@gmail.com>
2016-01-07 18:20:25 +0100

    Changed the material for the ground

--- shaders/bVertexShader.glsl
+++ shaders/bVertexShader.glsl
@@ -1,15 +1,19 @@
+precision mediump float;
+
 varying vec4 curvePos;
-varying float n;
+varying vec3 pos;
 
 void main() 
 {
-    vec3 pos = position;
+    float n;
+    pos = position;
     curvePos = modelMatrix * vec4(position, 1.0);
 
-    n  = snoise(vec3(pos.x*0.1,  pos.y*0.1, 1.0));
-    n += snoise(vec3(pos.x*0.2,  pos.y*0.2, 1.0)*0.5);
-    n += snoise(vec3(pos.x*0.4,  pos.y*0.4, 1.0)*0.25);
+    n  = snoise(vec3(pos.x*0.01, pos.y*0.01, 1.0));
+    n += snoise(vec3(pos.x*0.05,  pos.y*0.05, 1.0)*0.5);
+    //n += snoise(vec3(pos.x*0.1,  pos.y*0.1, 1.0)*0.25);
+    //n += snoise(vec3(pos.x*0.2,  pos.y*0.2, 1.0)*0.125);
 
-    curvePos.y = n*5.0;
+    curvePos.y = n*20.0;
     gl_Position = projectionMatrix * viewMatrix * curvePos;
 }
--- shaders/wFragmentShader.glsl
+++ shaders/wFragmentShader.glsl
@@ -1,9 +1,9 @@
+
 varying vec2 vUv;
-varying vec4 curvePos;
+
 
 void main() 
 {
-    //curvePos is passed through the vertex shader
     // color is RGBA: u, v, 0, 1
-    gl_FragColor = vec4( vec3( vUv, curvePos.y ), 1. );
+    gl_FragColor = vec4( vec3( vUv, 0.5 ), 1. );
 }
